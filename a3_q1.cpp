05ba85a5be92db00b835e3defd7fc8eeccdd7717
Anandita Chaudhary <121678604+Anni299@users.noreply.github.com>
2024-02-14 21:16:31 +0530

    Create a3_q1.cpp

---
+++ a3_q1.cpp
@@ -0,0 +1,18 @@
+// Peak element
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       for(int i = 0; i < n; i++){
+           if(i == 0){
+               if(arr[i] >= arr[i + 1]) return i;
+           }
+           if(i == n - 1){
+               if(arr[i] >= arr[i - 1]) return i;
+           }
+           if(arr[i] >= arr[i+1] && arr[i] >= arr[i - 1]) return i;
+       }
+    }
+};
